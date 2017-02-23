dd67da779a50830ee8552f12a58a597dbca7f40b
Benjamin Reed <ranger@opennms.org>
2017-02-23 16:34:06 -0500

    add @Override to hashCode

--- codegen/src/main/java/org/exolab/castor/builder/factory/SourceFactory.java
+++ codegen/src/main/java/org/exolab/castor/builder/factory/SourceFactory.java
@@ -1142,6 +1142,10 @@ public final class SourceFactory extends BaseFactory {
 //                + "<b>Effective Java Programming Language Guide</b> "
 //                + "by Joshua Bloch, Chapter 3");
 
+        if (getConfig().useJava50()) {
+            jMethod.addAnnotation(new JAnnotation(new JAnnotationType("Override")));
+        }
+
         // The hashCode method has no arguments
         jclass.addMethod(jMethod);
         JSourceCode jsc = jMethod.getSourceCode();
